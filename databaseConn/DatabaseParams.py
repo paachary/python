fb552195c4ccab8ebd289788ff92544cef649f62
Prashant Acharya <prashant_acharya74@yahoo.com>
2017-10-14 18:07:03 +0530

    Adding directory for flask

--- databaseConn/DatabaseParams.py
+++ databaseConn/DatabaseParams.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python
 from config import config
-import sys
 class DatabaseParams:
     'This is the parent class which has basic methods to get the db connection parameters'
-    dbType = 'mongodb';
+    dbType = 'mongodb'
 
     def __init__(self):
-        None;
+        None
 
     def setDbType(self, dbType):
-        self.dbType = dbType;
+        self.dbType = dbType
 
     def getDbType(self):
-        return self.dbType;
+        return self.dbType
 
     def getConnParameters(self):
         if (self.dbType is None ):
-            raise ValueError('The passed argument for dbType is null');
-        params = config("../config/database.ini", self.dbType);
+            raise ValueError('The passed argument for dbType is null')
+        params = config("../config/database.ini", self.dbType)
 
         if (params is None):
-            raise ValueError('The connection details in the ini file does not exist for dbType:', self.dbType);
-        return params;
+            raise ValueError('The connection details in the ini file does not exist for dbType:', self.dbType)
+        return params
         
