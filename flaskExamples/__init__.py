7959b427711460cfa3908ffb881226294ef5365a
Prashant Acharya <prashant_acharya74@yahoo.com>
2018-05-04 18:27:16 +0530

    cleanup

--- flaskExamples/__init__.py
+++ flaskExamples/__init__.py
@@ -1,9 +1,28 @@
 #from add_nums import add_nums
 #from number_reverse import NumberReverse
-from flask import Flask, json, jsonify, request, render_template, redirect, url_for, Response
+from flask import Flask, json, jsonify, request, render_template, redirect, url_for, Response, flash
 from DatabaseConnection import DatabaseConnection
 from rnd import get_exec_time
+import logging
+from logging.handlers import RotatingFileHandler
+from logging.config import dictConfig
 
+
+dictConfig({
+        'version': 1,
+        'formatters': {'default': {
+           'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+       }},
+       'handlers': {'wsgi': {
+           'class': 'logging.StreamHandler',
+           'stream': 'ext://flask.logging.wsgi_errors_stream',
+           'formatter': 'default'
+       }},
+       'root': {
+           'level': 'INFO',
+           'handlers': ['wsgi']
+      }
+})
 app = Flask(__name__, instance_relative_config=True, template_folder="data/templates")
 log_def = True
 
--- flaskExamples/data/__init__.py
+++ flaskExamples/data/__init__.py
@@ -1,4 +1,4 @@
-from flaskExamples import app
-from flaskExamples import jsonify, json, request, render_template, redirect,url_for, Response
+from flaskExamples import app, RotatingFileHandler
+from flaskExamples import jsonify, json, request, render_template, redirect,url_for, Response,  RotatingFileHandler, logging, flash
 from flaskExamples import DatabaseConnection
 from flaskExamples import get_exec_time
