63224dce6541ba33a0482243de1b232fb244010c
Prashant Acharya <prashant_acharya74@yahoo.com>
2018-05-04 08:13:12 +0530

    cleanup

--- flaskExamples/__init__.py
+++ flaskExamples/__init__.py
@@ -1,10 +1,10 @@
-from add_nums import add_nums
-from number_reverse import NumberReverse
+#from add_nums import add_nums
+#from number_reverse import NumberReverse
 from flask import Flask, json, jsonify, request, render_template, redirect, url_for, Response
 from DatabaseConnection import DatabaseConnection
 from rnd import get_exec_time
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__, instance_relative_config=True, template_folder="data/templates")
 log_def = True
 
 app.config.from_object('config_file')
